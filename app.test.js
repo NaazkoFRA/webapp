af7f3d6685fbfe7d3b9116698fb55eae88593c4b
NaazkoFRA <steeve.brillouet@edu.igensia.com>
2026-06-30 20:00:06 +0200

    Revise tests for calcul function
    Updated tests for the calcul function and removed unused tests.

--- app.test.js
+++ app.test.js
@@ -1,24 +1,6 @@
-const { calcul, app } = require('./app');
-const request = require('supertest');
-
-test('calcul additionne deux positifs', () => {
-  expect(calcul(1, 2)).toBe(3);
+const { calcul } = require('./app');
+ 
+test('2 + 3 = 5', () => {
+  expect(calcul(2, 3)).toBe(5);
 });
-
-test('calcul avec zéro', () => {
-  expect(calcul(0, 5)).toBe(5);
-});
-
-test('calcul avec négatifs', () => {
-  expect(calcul(-1, -2)).toBe(-3);
-});
-
-test('calcul mixte', () => {
-  expect(calcul(10, -3)).toBe(7);
-});
-
-test('GET / retourne Hello DevSecOps!', async () => {
-  const res = await request(app).get('/');
-  expect(res.statusCode).toBe(200);
-  expect(res.text).toBe('Hello DevSecOps!');
-});
+ 
