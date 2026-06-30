fc5a56fe2340ee801b193db3d247ed3547463319
NaazkoFRA <steeve.brillouet@edu.igensia.com>
2026-06-30 18:42:42 +0200

    Enhance tests with GET endpoint check
    Added a test for the GET endpoint to verify response.

--- app.test.js
+++ app.test.js
@@ -1,4 +1,5 @@
-const { calcul } = require('./app');
+const { calcul, app } = require('./app');
+const request = require('supertest');
 
 test('calcul additionne deux positifs', () => {
   expect(calcul(1, 2)).toBe(3);
@@ -14,4 +15,10 @@ test('calcul avec négatifs', () => {
 
 test('calcul mixte', () => {
   expect(calcul(10, -3)).toBe(7);
+});
+
+test('GET / retourne Hello DevSecOps!', async () => {
+  const res = await request(app).get('/');
+  expect(res.statusCode).toBe(200);
+  expect(res.text).toBe('Hello DevSecOps!');
 });
