e6c338ae5c39e27d2de33021b8e4e5383d80b7c3
NaazkoFRA <steeve.brillouet@edu.igensia.com>
2026-06-30 18:18:23 +0200

    fix: add tests to reach coverage threshold

--- app.test.js
+++ app.test.js
@@ -1,5 +1,17 @@
 const { calcul } = require('./app');
 
-test('calcul additionne correctement', () => {
+test('calcul additionne deux positifs', () => {
   expect(calcul(1, 2)).toBe(3);
+});
+
+test('calcul avec zéro', () => {
+  expect(calcul(0, 5)).toBe(5);
+});
+
+test('calcul avec négatifs', () => {
+  expect(calcul(-1, -2)).toBe(-3);
+});
+
+test('calcul mixte', () => {
+  expect(calcul(10, -3)).toBe(7);
 });
